c1e3bbc3733d2d695b5fddc0fab88c1e34e98502
XXNBY24029\Lenovo <455509712@qq.com>
2025-09-01 16:18:43 +0800

    use api post

---
+++ submit_step.py
@@ -0,0 +1,17 @@
+import requests
+import random
+import os
+
+url = 'https://gg.ibushu.com/ibushu/api/submitZepp'
+
+MIN_STEP = os.environ.get('MIN_STEP')
+MAX_STEP = os.environ.get('MAX_STEP')
+
+data = {
+    'zeppAccount': os.environ.get('ZEPP_ACCOUNT'),
+    'zeppPassword': os.environ.get('ZEPP_PASSWORD'),
+    'step': random.randint(MIN_STEP, MAX_STEP),
+}
+
+resp = requests.post(url=url, data=data)
+print(resp.status_code)
