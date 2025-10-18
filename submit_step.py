d041289343293ecdb0850496d4e3453eaeceda16
juniorny <35912735+juniorny@users.noreply.github.com>
2025-10-18 22:20:04 +0800

    Update submit_step.py

--- submit_step.py
+++ submit_step.py
@@ -13,5 +13,17 @@ data = {
     'step': random.randint(int(MIN_STEP), int(MAX_STEP)),
 }
 
-resp = requests.post(url=url, data=data)
-print(resp.text)
+try_time = 0
+while try_time < 10:
+    try:
+        resp = requests.post(url=url, data=data)
+        print(resp.text)
+        if "同步失败" in resp.text:
+            raise ValueError("同步失败")
+        elif "同步成功" in resp.text:
+            print("同步成功")
+            break
+    except:
+        try_time += 1
+        print(f"同步失败，开始第{try_time}重试...")
+        time.sleep(10)
